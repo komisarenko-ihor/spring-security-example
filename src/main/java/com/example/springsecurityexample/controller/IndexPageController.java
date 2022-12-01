82c29ea6e592b0596ddc329d4c09f8c4e599cc3b
Ihor Komisarenko <bulletss666@gmail.com>
2022-12-01 11:38:27 +0200

    3: HTTP Basic Auth: Testing Spring Security with JUnit5

--- src/main/java/com/example/springsecurityexample/controller/IndexPageController.java
+++ src/main/java/com/example/springsecurityexample/controller/IndexPageController.java
@@ -1,13 +1,16 @@
 package com.example.springsecurityexample.controller;
 
+import com.example.springsecurityexample.model.SimpleModel;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexPageController {
 
     @GetMapping({"", "/", "/index"})
-    public String getIndex() {
+    public String getIndex(Model model) {
+        model.addAttribute("simple_model", new SimpleModel("Index page"));
         return "index";
     }
 }
---
+++ src/test/java/com/example/springsecurityexample/controller/IndexPageControllerIT.java
@@ -0,0 +1,40 @@
+package com.example.springsecurityexample.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest
+public class IndexPageControllerIT {
+
+    @Autowired
+    WebApplicationContext wac;
+
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(wac)
+                .apply(springSecurity())
+                .build();
+    }
+
+    @WithMockUser("spring")
+    @Test
+    void getIndexPage() throws Exception {
+        mockMvc.perform(get("/index"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"))
+                .andExpect(model().attributeExists("simple_model"));
+    }
+}
