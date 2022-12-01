5767bec79e7edc6667bef684bd86feec5df92397
Ihor Komisarenko <bulletss666@gmail.com>
2022-12-01 11:42:20 +0200

    3: HTTP Basic Auth: Testing HTTP Basic Auth

--- src/test/java/com/example/springsecurityexample/controller/IndexPageControllerIT.java
+++ src/test/java/com/example/springsecurityexample/controller/IndexPageControllerIT.java
@@ -9,6 +9,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -33,6 +34,14 @@ public class IndexPageControllerIT {
     @Test
     void getIndexPage() throws Exception {
         mockMvc.perform(get("/index"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"))
+                .andExpect(model().attributeExists("simple_model"));
+    }
+
+    @Test
+    void getIndexPageWithHttpBasic() throws Exception {
+        mockMvc.perform(get("/index").with(httpBasic("user1", "password1")))
                 .andExpect(status().isOk())
                 .andExpect(view().name("index"))
                 .andExpect(model().attributeExists("simple_model"));
