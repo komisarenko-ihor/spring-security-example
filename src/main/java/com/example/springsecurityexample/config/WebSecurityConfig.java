309b96a592f15c43b2b132939d17a01dbf1780a3
Ihor Komisarenko <bulletss666@gmail.com>
2022-12-02 19:50:07 +0200

    5: In Memory Authentication Provider: In Memory Authentication Fluent API

--- src/main/java/com/example/springsecurityexample/config/WebSecurityConfig.java
+++ src/main/java/com/example/springsecurityexample/config/WebSecurityConfig.java
@@ -1,13 +1,11 @@
 package com.example.springsecurityexample.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
@@ -33,20 +31,15 @@ public class WebSecurityConfig {
                 .and().build();
     }
 
-    @Bean
-    protected UserDetailsService userDetailsService() {
-        UserDetails admin = User.withDefaultPasswordEncoder()
-                .username("user1")
-                .password("password1")
+    @Autowired
+    public void authenticationManager(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user1")
+                .password("{noop}password1")
                 .roles("ADMIN")
-                .build();
-
-        UserDetails user = User.withDefaultPasswordEncoder()
-                .username("user2")
-                .password("password2")
-                .roles("USER")
-                .build();
-
-        return new InMemoryUserDetailsManager(admin, user);
+                .and()
+                .withUser("user2")
+                .password("{noop}password2")
+                .roles("USER");
     }
 }
