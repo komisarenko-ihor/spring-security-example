e80f6d783a565ea63ed367cd13bfa1e6fe0f5886
Ihor Komisarenko <bulletss666@gmail.com>
2023-02-09 17:10:42 +0200

    8: Database Authentication: Spring Data JPA Repositories

---
+++ src/main/java/com/example/springsecurityexample/repositories/security/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.springsecurityexample.repositories.security;
+
+import com.example.springsecurityexample.domain.security.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findByUsername(String username);
+}
