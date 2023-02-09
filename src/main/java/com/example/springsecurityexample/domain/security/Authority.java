d1ee63d60e69f3223c6383fe759a940a01a2c9dc
Ihor Komisarenko <bulletss666@gmail.com>
2023-02-09 17:04:28 +0200

    8: Database Authentication: Project Lombok Configuration

--- src/main/java/com/example/springsecurityexample/domain/security/Authority.java
+++ src/main/java/com/example/springsecurityexample/domain/security/Authority.java
@@ -1,8 +1,15 @@
 package com.example.springsecurityexample.domain.security;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.util.Set;
 
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @Entity
 public class Authority {
 
