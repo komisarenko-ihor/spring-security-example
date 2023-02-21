1f44eca191059698b9fe0707921dabda443aac53
Ihor Komisarenko <bulletss666@gmail.com>
2023-02-21 14:38:50 +0200

    15: User Lockout Using Spring Security Events: Configuration of Spring Security Event Publisher

---
+++ src/main/java/com/example/springsecurityexample/security/listeners/AuthenticationSuccessListener.java
@@ -0,0 +1,18 @@
+package com.example.springsecurityexample.security.listeners;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class AuthenticationSuccessListener {
+
+    @EventListener
+    public void listen(AuthenticationSuccessEvent event) {
+
+        log.debug("User Logged In: " + event.getAuthentication().getName());
+
+    }
+}
