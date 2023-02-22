1e32b6f257f93da451b1423d53c8feed9f21a184
Ihor Komisarenko <bulletss666@gmail.com>
2023-02-22 18:42:18 +0200

    16: Two Factor Authentication: Configure Google Secret Persistence

---
+++ src/main/java/com/example/springsecurityexample/security/google/GoogleCredentialRepository.java
@@ -0,0 +1,32 @@
+package com.example.springsecurityexample.security.google;
+
+import com.example.springsecurityexample.domain.security.User;
+import com.example.springsecurityexample.repositories.security.UserRepository;
+import com.warrenstrange.googleauth.ICredentialRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class GoogleCredentialRepository implements ICredentialRepository {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public String getSecretKey(String username) {
+        User user = userRepository.findByUsername(username).orElseThrow();
+        return user.getGoogle2FaSecret();
+    }
+
+    @Override
+    public void saveUserCredentials(String username, String secretKey, int validationCode, List<Integer> scratchCodes) {
+        User user = userRepository.findByUsername(username).orElseThrow();
+        user.setGoogle2FaSecret(secretKey);
+        user.setUserGoogle2Fa(true);
+        userRepository.save(user);
+    }
+}
