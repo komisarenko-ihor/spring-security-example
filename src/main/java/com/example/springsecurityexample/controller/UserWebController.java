52d9f3ba8a413b0aa7fe8dcbef77c44a35fb5aec
Ihor Komisarenko <bulletss666@gmail.com>
2023-02-22 18:56:57 +0200

    16: Two Factor Authentication: Generate QR Code for Google Authenticator

--- src/main/java/com/example/springsecurityexample/controller/UserWebController.java
+++ src/main/java/com/example/springsecurityexample/controller/UserWebController.java
@@ -1,8 +1,12 @@
 package com.example.springsecurityexample.controller;
 
+import com.example.springsecurityexample.domain.security.User;
 import com.example.springsecurityexample.repositories.security.UserRepository;
+import com.warrenstrange.googleauth.GoogleAuthenticator;
+import com.warrenstrange.googleauth.GoogleAuthenticatorQRGenerator;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,11 +21,19 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class UserWebController {
 
     private final UserRepository userRepository;
+    private final GoogleAuthenticator googleAuthenticator;
 
     @GetMapping("/register2fa")
     public String register2fa(Model model) {
 
-        model.addAttribute("googleurl", "todo");
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        String url = GoogleAuthenticatorQRGenerator.getOtpAuthURL("SpringSecurityExample", user.getUsername(),
+                googleAuthenticator.createCredentials(user.getUsername()));
+
+        log.debug("Google QR URL: " + url);
+
+        model.addAttribute("googleurl", url);
 
         return "user/register2fa";
     }
